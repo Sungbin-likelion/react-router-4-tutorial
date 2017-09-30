009d698a38918f31b1b4ca185aaa0a78af9e6f2f
velopert <public.velopert@gmail.com>
2017-09-30 18:31:04 +0900

    initial commit

---
+++ src/pages/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Post = ({location, match}) => {
+  console.log('Post:', match);
+
+  return (
+    <p>
+      포스트 #{match.params.id}
+    </p>
+  );
+};
+
+export default Post;
+
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+/* 아래 코드들은 컴포넌트를 불러온다음에
+   동일한 이름으로 내보내줍니다. */
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Post } from './Post';
+export { default as Posts } from './Posts';
