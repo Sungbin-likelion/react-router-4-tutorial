091b78df7907e9b4872fa8ae841f28018c6737c4
velopert <public.velopert@gmail.com>
2017-10-06 01:39:39 +0900

    code split

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import {
   Home,
   About,
   Posts
-} from 'pages';
+} from 'pages/index.async.js';
 import Menu from 'components/Menu';
-import AsyncSplitMe from 'components/AsyncSplitMe';
 
 
 const App = () => {
   return (
     <div>
       <Menu/>
-      <AsyncSplitMe/>
       <Route exact path="/" component={Home}/>
       <Route path="/about/:name?" component={About}/>
       <Route path="/posts" component={Posts}/>
